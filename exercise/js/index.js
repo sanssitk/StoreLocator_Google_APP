f8ab7bb16863d243b9d93ba80a36d66cc2af09b6
sanjay <sanssitk@gmail.com>
2021-07-06 17:25:23 -0500

    html layout finalize

---
+++ exercise/js/index.js
@@ -0,0 +1,12 @@
+// require("dotenv").config();
+// let map;
+
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById("map"), {
+//         center: {
+//             lat: -34.397,
+//             lng: 150.644
+//         },
+//         zoom: 8,
+//     });
+// }
